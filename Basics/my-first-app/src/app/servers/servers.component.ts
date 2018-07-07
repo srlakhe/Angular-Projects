41bbe4ce087723a4ef0066ef8d77f79ea4f451b5
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-07-07 22:05:25 +0530

    Add files via upload

---
+++ Basics/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '.app-servers',
+  // selector: '[app-servers]
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'test server';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+}
